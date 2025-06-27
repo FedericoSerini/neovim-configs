10a4435e34e407aa672db3092bad926379ab6948
FedericoSerini <federico.serini.se@pec.it>
2025-06-27 16:28:29 +0200

    update: color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme kanagawa-lotus]])
-    end,
-  },
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 0,
+		config = function()
+			vim.cmd([[colorscheme kanagawa-lotus]])
+		end,
+	},
+	{
+		"colorscheme github_dark_dimmed",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme kanagawa-lotus]])
+		end,
+	},
 }
